e64148ab86616568d3482eb4c0096d4c3df13b88
Daniel Huber <dhub100@gmail.com>
2025-05-16 09:42:27 +0200

    Added 3 month Libor data and adjusted the rest of the script including the graphs.

--- scripts/01_load_data.R
+++ scripts/01_load_data.R
@@ -12,6 +12,7 @@ library(zoo)
 policy_rate_data <- read_excel("data/snb-data-snbgwdzid-en-all-20250414_1000.xlsx",
                                col_types = c("text", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"),
                                skip = 21)
+
 inflation_data <- read_excel("data/snb-data-plkoprinfla-en-all-20250422_0900.xlsx",
                              skip = 14) #skipping the first 14 rows
 
@@ -47,11 +48,47 @@ ggplot(pr_full, aes(x = date)) +
   theme(legend.position = "bottom", legend.direction = "horizontal") 
 
 
+libor_data <- suppressWarnings(
+  read_excel("data/snb-target rate-policy rate-2000-2025.xlsx",
+                         range = cell_limits(c(18, 1), c(NA, 4)),
+                         col_names = c("date", "policy_rate", "libor_3m_low", "libor_3m_high"),
+                         col_types = c("text", "numeric", "numeric", "numeric")) %>%
+  mutate(date = ymd(str_c(date, "-01")),
+         (across(c(policy_rate, libor_3m_low, libor_3m_high),
+                 ~ round(as.numeric(.), 2))),
+         libor_3m_avg = (libor_3m_low + libor_3m_high) / 2) %>% 
+  mutate(libor_3m_avg = round(libor_3m_avg, 2))
+)
+
+ggplot(libor_data, aes(x = date)) +
+  geom_line(aes(y = policy_rate, color = "Policy Rate")) +
+  geom_line(aes(y = libor_3m_low, color = "libor_3m_lower")) +
+  geom_line(aes(y = libor_3m_high, color = "libor_3m_upper")) +
+  geom_line(aes(y = libor_3m_avg, color = "libor_3m_avg")) +
+  scale_color_manual(values = c("Policy Rate" = "blue",
+                                "libor_3m_lower" = "green",
+                                "libor_3m_upper" = "brown",
+                                "libor_3m_avg" = "lightblue")) +
+  labs(title = "Swiss Policy Rates, 3 Month Libor lower and upper limits",
+       color = "Legend",
+       y = "(Policy-Rates") +
+  theme_minimal() +
+  theme(legend.position = "bottom", legend.direction = "horizontal") 
+
+# The average of the 3 Month Libor lower and upper limits is the correct proxy for the
+# SNB policy rate before 2020.
+
+
+
 # Prepare data
 
-pr <- pr_full %>% 
-  select(date, ir_above, special) %>%
-  filter(!is.na(special))                 # remove rows with NA in ir_above
+pr <- libor_data %>%
+  mutate(
+  policy_rate = if_else(
+    date < as.Date("2019-06-01"),
+    libor_3m_avg,       
+    policy_rate)) %>% 
+    select(date, policy_rate)
 
 infl <- inflation_data %>% 
   as_tibble() %>%
@@ -59,24 +96,22 @@ infl <- inflation_data %>%
   mutate(date = ymd(str_c(date, "-01")),   # add a '-01' to the date string before making it a date
          infl = as.numeric(infl),
          infl = round(infl, 1))
-# view(infl)
 
 
 # Group by year-month and keep the first (oldest) row in each group
 
-pr <- pr %>%                      
-  group_by(year_month = floor_date(date, "month")) %>%     # Group by month
-  slice_min(date) %>%             # Keep oldest date in each month
-  ungroup() %>%
-  mutate(date = year_month) %>%   # Overwrite 'date' with YYYY-MM-01
-  select(-year_month)             # Remove temporary column
+# pr <- pr %>%
+#   group_by(year_month = floor_date(date, "month")) %>%     # Group by month
+#   slice_min(date) %>%             # Keep oldest date in each month
+#   ungroup() %>%
+#   mutate(date = year_month) %>%   # Overwrite 'date' with YYYY-MM-01
+#   select(-year_month)             # Remove temporary column
 # view(pr)
 
 
 # merge data
 
 df <- inner_join(pr, infl, by = "date")  # merge the two tibbles
-view(df)
 
 
 # convert df to a zoo time series object
@@ -85,20 +120,18 @@ df_ts <- zoo(
   df %>% select(-date),
   order.by = df$date
 )
-view(df_ts)
 
 
 ## Visualize data
 
 ggplot(df, aes(x = date)) +
-  geom_line(aes(y = ir_above, color = "Interest rate on sight deposits above threshold")) +
-  geom_line(aes(y = special, color = "Special rate Liquidity-shortage financing facility")) +
+  geom_line(aes(y = policy_rate, color = "SNB Policy Rate")) +
   geom_line(aes(y = infl, color = "SNB - Core inflation, trimmed mean")) +
-  scale_color_manual(values = c("Interest rate on sight deposits above threshold" = "blue",
-                                "Special rate Liquidity-shortage financing facility" = "darkgreen",
+  scale_color_manual(values = c("SNB Policy Rate" = "blue",
                                 "SNB - Core inflation, trimmed mean" = "red")) +
-  labs(title = "Swiss Policy Rates and Inflation Rates 2004-2025",
+  labs(title = "Swiss Policy Rates and Inflation Rates 2000-2025",
        color = "Legend",
        y = "(Inflation- / Policy-) Rates") +
   theme_minimal() +
   theme(legend.position = "bottom", legend.direction = "horizontal")
+
